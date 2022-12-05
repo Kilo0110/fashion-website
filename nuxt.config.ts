1d2109481887bd085de5b26a393c27b0289fbc24
KamCy Ahaka <kamcyahaka@gmail.com>
2022-12-05 09:37:51 +0100

    Added Pinia to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+  ],
   css: ['@fortawesome/fontawesome-svg-core/styles.css'],
+  /* piniaPersistedstate: {
+    cookieOptions: {
+      sameSite: 'strict',
+    },
+    storage: 'localStorage',
+  }, */
 });
