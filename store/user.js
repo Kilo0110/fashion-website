5b73ab4da12648e586da9e2dd449aa02d5bff160
KamCy Ahaka <kamcyahaka@gmail.com>
2022-12-21 22:43:57 +0100

    Updated user store to add authentication errors

--- store/user.js
+++ store/user.js
@@ -4,15 +4,15 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-  onAuthStateChanged,
 } from 'firebase/auth';
 
 import { defineStore, skipHydrate } from 'pinia';
-import { StorageSerializers, useLocalStorage } from '@vueuse/core';
+import { useLocalStorage } from '@vueuse/core';
 
 export const useUserStore = defineStore('user', {
   state: () => ({
     user: useLocalStorage('user'),
+    authenticationError: null,
   }),
   actions: {
     async signUserUp(email, password) {
@@ -20,9 +20,8 @@ export const useUserStore = defineStore('user', {
         const res = await createUserWithEmailAndPassword(auth, email, password);
         const data = res.user;
         this.user = data;
-        return navigateTo('/profile/');
       } catch (error) {
-        console.log(error);
+        this.authenticationError = error.code;
       }
     },
     async signUserIn(email, password) {
@@ -30,19 +29,20 @@ export const useUserStore = defineStore('user', {
         const res = await signInWithEmailAndPassword(auth, email, password);
         const data = res.user;
         this.user = data;
-        return navigateTo('/profile/');
       } catch (error) {
-        console.log(error);
+        this.authenticationError = error.code;
       }
     },
     async logUserOut() {
       try {
         const res = await signOut(auth);
         this.user = null;
-        return navigateTo('/');
       } catch (error) {
-        console.log(error);
+        this.authenticationError = error.code;
       }
+    },
+    resetAuthenticationError() {
+      this.authenticationError = null;
     },
   },
   persist: ['user'],
