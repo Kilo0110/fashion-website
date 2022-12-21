ba2ddd072c96e0bf29c93eb8304f5144052f6d84
KamCy Ahaka <kamcyahaka@gmail.com>
2022-12-21 23:57:37 +0100

    Added password toggle feature

--- components/Input.vue
+++ components/Input.vue
@@ -7,37 +7,51 @@
       :for="props.inputName"
       >{{ inputLabel }}</label
     >
-    <input
-      class="text-sm text-black rounded-lg"
-      :type="props.inputType"
-      :name="props.inputName"
-      :placeholder="props.inputPlaceholder"
-      :value="props.modelValue"
-      @input="(event) => emits('update:modelValue', event.target.value)"
-    />
+    <template v-if="!props.isPassword">
+      <input
+        class="text-sm text-black rounded-lg"
+        :type="props.inputType"
+        :name="props.inputName"
+        :placeholder="props.inputPlaceholder"
+        :value="props.modelValue"
+        @input="(event) => emits('update:modelValue', event.target.value)"
+      />
+      <client-only>
+        <font-awesome-icon
+          :icon="props.isValid ? ['fas', 'check'] : ['fas', 'xmark']"
+          class="absolute -translate-y-1/2 bg-white top-1/2 right-4"
+          :class="props.isValid ? 'text-green-500' : 'text-red-500'"
+        />
+      </client-only>
+    </template>
     
-    <client-only v-if="!props.isPassword">
-      <font-awesome-icon
-        :icon="props.isValid ? ['fas', 'check'] : ['fas', 'xmark']"
-        class="absolute -translate-y-1/2 bg-white top-1/2 right-4"
-        :class="props.isValid ? 'text-green-500' : 'text-red-500'"
+    <template v-else>
+      <input
+        class="text-sm text-black rounded-lg"
+        :type="fieldType"
+        :name="props.inputName"
+        :placeholder="props.inputPlaceholder"
+        :value="props.modelValue"
+        @input="(event) => emits('update:modelValue', event.target.value)"
       />
-    </client-only>
+      <client-only>
+        <font-awesome-icon @click="makePasswordVisible"
+          :icon="showPasswordIcon ? ['fas', 'eye-slash'] : ['fas', 'eye']"
+          class="absolute -translate-y-1/2 bg-white top-1/2 right-4 cursor-pointer"
+        />
+      </client-only>
+  
+      <div v-if="props.isPassword" class="mt-1 password-error-message text-xs italic text-red-500 w-full">
+        {{props.passwordError}}
+      </div>
+    </template>
 
-    <client-only v-if="props.isPassword">
-      <font-awesome-icon @click="makePasswordVisible"
-        :icon="showPassword ? ['fas', 'eye-slash'] : ['fas', 'eye']"
-        class="absolute -translate-y-1/2 bg-white top-1/2 right-4 cursor-pointer"
-      />
-    </client-only>
-
-    <div v-if="props.isPassword" class="mt-1 password-error-message text-xs italic text-red-500 w-full">
-      {{props.passwordError}}
-    </div>
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+
 const emits = defineEmits(['update:modelValue']);
 const props = defineProps({
   inputName: {
@@ -72,14 +86,23 @@ const props = defineProps({
     type: String,
   },
 });
-const showPassword = ref(true)
+const showPasswordIcon = ref(true)
+const fieldType = ref('text')
 
 const makePasswordVisible = () => {
-  showPassword.value = !showPassword.value
-  if (props.inputType === 'password') {
-    props.inputType = 'text'
-  } else props.inputType = 'password'
+  showPasswordIcon.value = !showPasswordIcon.value
+  if (fieldType.value === 'password') {
+    fieldType.value = 'text'
+  } else fieldType.value = 'password'
 }
+
+watch(props.inputType, (type) => {
+  fieldType.value = type
+})
+
+onMounted(() => [
+  fieldType.value = props.inputType
+])
 </script>
 
 <style lang="scss" scoped>
