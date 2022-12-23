f39e7a28b884fca724cb867f3e6b5ae440df6fc4
KamCy Ahaka <kamcyahaka@gmail.com>
2022-12-23 21:11:45 +0100

    Removed white background from NormalHeader

--- layouts/NormalHeader.vue
+++ layouts/NormalHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="header">
-    <nav class="nav flex justify-center relative items-center gap-5 w-full min-h-[10vh] px-3 bg-white">
+    <nav class="nav flex justify-center relative items-center gap-5 w-full min-h-[10vh] px-3">
       <BackButton additionalClasses="absolute top-1/2 -translate-y-1/2 left-2" />
       <h1 class="font-bold" :class="props.alignment">{{ props.headerTitle }}</h1>
     </nav>
