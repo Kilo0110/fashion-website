5a3eefb19114a9873d3e6c17912a1cb2e8cbd7e4
KamCy Ahaka <kamcyahaka@gmail.com>
2022-12-23 23:02:39 +0100

    Made button text bold and black

--- components/CallToActionBtn.vue
+++ components/CallToActionBtn.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="handleClick" class="block w-full text-white text-center text-xs py-3 uppercase rounded-full shadow-md transition-all duration-300 active:scale-90" :class="props.isColored ? 'bg-red-600' : 'bg-transparent border-2 border-white hover:bg-red-600 hover:border-red-600'">
+  <button @click="handleClick" class="font-bold block w-full text-white text-center text-xs py-3 uppercase rounded-full shadow-md transition-all duration-300 active:scale-90" :class="props.isColored ? 'bg-red-600' : 'bg-transparent border-2 border-white hover:bg-red-600 hover:border-red-600 text-black'">
     {{ props.buttonText }}
   </button>
 </template>
