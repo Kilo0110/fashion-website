77eb2c7cc12f71826baf3817947565dbb6f79d05
KamCy Ahaka <kamcyahaka@gmail.com>
2023-01-03 01:36:11 +0100

    Changed the auth state checker for the user-auth middleware

--- middleware/user-auth.js
+++ middleware/user-auth.js
@@ -1,11 +1,13 @@
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '~~/firebase/config.js';
+// import { auth } from '~~/firebase/index.js';
+import { useUserStore } from '~~/store/user';
+
+const router = useRouter();
+const userStore = useUserStore();
 
 export default defineNuxtRouteMiddleware((to, from) => {
-  // get the current user state
-  onAuthStateChanged(auth, (user) => {
-    if (!user) {
-      return navigateTo('/auth/');
-    }
-  });
+  const user = userStore.user;
+  if (user === null || user === undefined || user === 'undefined') {
+    return navigateTo('/auth/');
+  }
 });
