cc990fe1de0fb83661dee3e3e6247a5f56006a96
KamCy Ahaka <kamcyahaka@gmail.com>
2023-02-16 09:23:05 +0100

    Connected Firebase emulator

--- firebase/index.js
+++ firebase/index.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +19,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
+connectAuthEmulator(auth, 'http://localhost:9099');
+
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
+connectFirestoreEmulator(db, 'localhost', 8080);
 
 export { auth, db };
